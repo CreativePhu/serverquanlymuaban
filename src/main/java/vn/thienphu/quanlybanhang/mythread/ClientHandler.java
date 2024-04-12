89887223454d78af9d1981b3d661edb9e777c2ff
thienphu <phutot1111@gmail.com>
2024-04-13 00:25:02 +0700

    first commit

---
+++ src/main/java/vn/thienphu/quanlybanhang/mythread/ClientHandler.java
@@ -0,0 +1,53 @@
+package vn.thienphu.quanlybanhang.mythread;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
+public class ClientHandler implements Runnable {
+	private Socket socket;
+	
+	
+	public ClientHandler(Socket socket) {
+		this.socket = socket;
+	}
+
+
+	@Override
+	public void run() {
+		try {
+			BufferedReader read = new BufferedReader(
+					new InputStreamReader(socket.getInputStream()));
+			BufferedWriter write = new BufferedWriter(
+					new OutputStreamWriter(socket.getOutputStream()));
+			
+			while(true) {
+				
+				String request = read.readLine();
+				System.out.println("Client " + socket.getLocalAddress().getHostAddress() + " " + request);
+				
+				String newResponse = request.toUpperCase();
+				
+				write.append(newResponse);
+				write.newLine();
+				write.flush();
+				
+				if(socket.isClosed()) {
+					System.out.println("Client " + socket.getLocalAddress().getHostAddress() + "disconnect");
+					read.close();
+					write.close();
+					break;
+				}
+			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+}
---
+++ src/main/java/vn/thienphu/quanlybanhang/run/Main.java
@@ -0,0 +1,30 @@
+package vn.thienphu.quanlybanhang.run;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import vn.thienphu.quanlybanhang.mythread.ClientHandler;
+
+public class Main {
+	public static void main(String[] args) {
+		try {
+			ServerSocket serverSocket = new ServerSocket(1234);
+			ExecutorService executorService = Executors.newCachedThreadPool();
+			while (true) {
+				
+				System.out.println("Đang chờ kết nối");
+				Socket socketClient = serverSocket.accept();
+				System.out.println("client " +  socketClient.getInetAddress().getHostAddress() + " is connect");
+				
+				ClientHandler clientHandler = new ClientHandler(socketClient);
+				executorService.execute(clientHandler);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
